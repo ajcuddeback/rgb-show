4ff304595e475db709cd35a893dc5c8184092fdc
ajcuddeback <ajcuddeback@gmail.com>
2023-11-26 17:10:51 -0500

    fixing threading

--- server/raspberry.py
+++ server/raspberry.py
@@ -1,9 +1,10 @@
 import threading
-from animations import shutdown
+from animations import shutdown  # Assuming you have a shutdown function in animations module
 
 class RaspberryThread(threading.Thread):
     def __init__(self, function):
         self.paused = True
+        self.should_abort = False  # Flag to indicate whether to abort the current function
         self.state = threading.Condition()
         self.function = function
         super(RaspberryThread, self).__init__()
@@ -12,14 +13,19 @@ class RaspberryThread(threading.Thread):
         super(RaspberryThread, self).start()
 
     def run(self):
-        # self.resume() #unpause self
+        # self.resume() # unpause self
         while True:
             with self.state:
                 if self.paused:
-                    self.state.wait() #block until notifed
+                    self.state.wait()  # block until notified
+                    shut_off_lights()
             while not self.paused:
-                # Call function 
+                self.should_abort = False  # Reset the abort flag
+                # Call function
                 self.function()
+                with self.state:
+                    if self.should_abort:
+                        break  # Break out of the loop if the abort flag is set
 
     def resume(self):
         with self.state:
@@ -29,3 +35,8 @@ class RaspberryThread(threading.Thread):
     def pause(self):
         with self.state:
             self.paused = True
+            self.should_abort = True  # Set the abort flag
+
+    def shut_off_lights(self):
+        # Add your code to shut off the lights (using the shutdown function or any other method)
+        shutdown()
