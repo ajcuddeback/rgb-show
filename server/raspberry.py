d16f1116c2b3f513cd9246366abd3b7ccbd701c2
ajcuddeback <ajcuddeback@gmail.com>
2023-11-26 17:26:08 -0500

    fixing threading

--- server/raspberry.py
+++ server/raspberry.py
@@ -13,7 +13,7 @@ class RaspberryThread(threading.Thread):
         self.paused = False
         super(RaspberryThread, self).start()
 
-     def run(self):
+    def run(self):
         while True:
             with self.state:
                 if self.paused:
