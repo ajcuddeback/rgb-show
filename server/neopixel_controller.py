8e1d15ee58f937e5717f837cb9274f577b49a4f3
ajcuddeback <ajcuddeback@gmail.com>
2023-11-27 11:40:29 -0500

    Trying out different api

--- server/neopixel_controller.py
+++ server/neopixel_controller.py
@@ -20,4 +20,4 @@ class NeoPixelController:
     def turn_off_all_lights(self):
         print("TURNING THE LIGHTS OFF NOW IN CONTROLLER")
         self.pixels.fill((0,0,0))
-	    self.pixels.show()
+        self.pixels.show()
