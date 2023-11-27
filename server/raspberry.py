3624c005e50f9e64f0b9c19185f0b3ae04081cfc
ajcuddeback <ajcuddeback@gmail.com>
2023-11-26 20:19:49 -0500

    Fixing logic in threads

--- server/raspberry.py
+++ server/raspberry.py
@@ -24,7 +24,7 @@ class RaspberryThread(threading.Thread):
 
             if not self.loop:
                 run_count = 0
-                if self.paused or run_count >=ß self.max_runs:
+                if self.paused or run_count >= self.max_runs:
                     continue
                     print("Continuing")
                 self.function()
