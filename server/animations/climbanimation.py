b25bb25e55483d4de0efa81713a4fbe37d9648db
ajcuddeback <ajcuddeback@gmail.com>
2023-11-29 12:48:57 -0500

    Fixing abstract class

--- server/animations/climbanimation.py
+++ server/animations/climbanimation.py
@@ -7,15 +7,15 @@ class climbanimation(AbstractAnimation):
         super().__init__(neo_pixel_controller, color)
         self.axis = [[0, 29], [30, 49], [50, 64], [65, 74], [75, 84], [85, 94], [95, 99]]
         self.colors = [(255,20,217), (0,255,0), (255,0,0)]
-        self.lock = threading.Lock()
+        self.animation_lock = threading.Lock()
     
     def run_animation(self):
-        with self.lock:
+        with self.animation_lock:
             self.is_running = True
         while True:
             self.fillup()
             print(f"runnimng {self.is_running}")
-            with self.lock:
+            with self.animation_lock:
                 if not self.check_if_is_running():
                     return
 
@@ -23,7 +23,7 @@ class climbanimation(AbstractAnimation):
         for i in range(len(self.axis)):
             for j in range(self.axis[i][0], self.axis[i][1]):
                 self.controller.pixels[j] = color
-                with self.lock:
+                with self.animation_lock:
                     if not self.check_if_is_running():
                         return
             if not self.check_if_is_running():
@@ -36,7 +36,7 @@ class climbanimation(AbstractAnimation):
     def fillup(self):
         for color in self.colors:
             self.climb(color)
-            with self.lock:
+            with self.animation_lock:
                 if not self.check_if_is_running():
                     return
         time.sleep(0.3)
