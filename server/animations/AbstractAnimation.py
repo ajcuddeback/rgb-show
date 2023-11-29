402086558b994ff5017c8628fa6295ffdf598c8c
ajcuddeback <ajcuddeback@gmail.com>
2023-11-29 12:56:52 -0500

    Fixing abstract class

--- server/animations/AbstractAnimation.py
+++ server/animations/AbstractAnimation.py
@@ -2,7 +2,6 @@ from abc import ABC, abstractmethod
 
 class AbstractAnimation(ABC):
     def __init__(self, neo_pixel_controller, color, *args, **kwargs):
-        super().__init__(*args, **kwargs)
         self.controller = neo_pixel_controller
         self.color = color
         self.is_running = False
