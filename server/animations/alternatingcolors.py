ac2fbc8f73ed4a91dc550cd68263bf767c3bef60
ajcuddeback <ajcuddeback@gmail.com>
2023-12-11 18:02:10 -0500

    debugging

--- server/animations/alternatingcolors.py
+++ server/animations/alternatingcolors.py
@@ -39,6 +39,7 @@ class alternatingcolors(AbstractAnimation):
             for j in range(self.controller.pixels.n):
                 print(f'offset: {offset} len: {len(self.colors)} j: {j}')
                 color_index = j + offset % len(self.colors)
+                print(f'Addition working??: {j + offset}')
                 print(f'RESULT? : {j % len(self.colors)}')
                 print(f'color_index {color_index}')
                 self.controller.pixels[i] = self.colors[color_index]
